c71e20d1f40f1ac619f74a89876163ed507817ce
slendyG <josuegarza0608@gmail.com>
2022-11-18 00:40:31 -0800

    changes to database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,12 +14,29 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('put to database')
-}
+  console.log('put to database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('data saved to database!', result.value);
+};
 
 // console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('--data retreived from database', result.value)
+    : console.log('-- data not found in database');
+    return result?.value;
+};
 
 initdb();
